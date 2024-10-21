3906af05da5e006a4e0231d34a007699dc241c6c
Retema2200 <s125145757@gmail.com>
2024-10-21 17:06:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,19 @@ self.addEventListener('install', event => {
           '/styles.css',
           '/manifest.json',
           '/images/icon-192.png',
-          '/images/icon-512.png'
+          '/images/icon-512.png',
+          '/images/icon-144x144.png', 
+          '/images/screenshot1-wide.png',  
+          '/images/screenshot2-narrow.png'
         ]);
       })
     );
-  });
-  
-  self.addEventListener('fetch', event => {
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(response => {
         return response || fetch(event.request);
       })
     );
-  });
-  
+});
