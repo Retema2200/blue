65cfdfab42592b56bdc208b0a3834eb84b8a34a6
Retema2200 <s125145757@gmail.com>
2024-11-25 17:21:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ function handleNotification(event) {
       const humValue = (value[3] | (value[4] << 8)) / 10;
       console.log(`溫度數據: ${tempValue.toFixed(1)}°C`);
       console.log(`濕度數據: ${humValue.toFixed(1)}%`);
-
       statusDisplay.textContent = `目前溫度：${tempValue.toFixed(1)}°C，濕度：${humValue.toFixed(1)}%`;
-    
+
+      message = `溫度：${tempValue.toFixed(1)}°C，濕度：${humValue.toFixed(1)}%`;
     
       break;
     case 0x02: // 血氧
@@ -38,21 +38,16 @@ function handleNotification(event) {
     const hbrValue = (value[3] | (value[4] << 8));
     console.log(`血氧數據: ${bldValue}%`);
     console.log(`心率數據: ${hbrValue} BPM`);
-
-
-
-
-
-
     statusDisplay.textContent = `目前血氧：${bldValue}% ，心率：${hbrValue} BPM`;
 
+    message = `血氧：${bldValue}% 心率：${hbrValue} BPM`;
       break;
     case 0x03: // 壓力
       const glvValue = value[1] | (value[2] << 8); // 合併低位和高位
       const relglv = glvValue / 10;
       console.log(`壓力數據: ${relglv} hpa`);
-
       statusDisplay.textContent = `目前壓力值：${relglv.toFixed(1)} hpa`; 
+
       break;
     case 0x04: // 光照值
       const dataValue = value[1] | (value[2] << 8); // 合併低位和高位
@@ -65,7 +60,7 @@ function handleNotification(event) {
     default:
       statusDisplay.textContent = '未知數據類型';
   }
-
+  sendLineNotify(message);
 }
 
 
@@ -182,3 +177,33 @@ document.querySelector('#measureOxygenButton').addEventListener('click', async (
     statusDisplay.textContent = "量測失敗，請重試";
   }
 }); 
+
+
+
+async function sendLineNotify(message) {
+  const token = 'ZLnjC4GWz4L6MAcKzlB6z6JLNxkfojHjJAUksYrJhND';
+  const url = 'https://notify-api.line.me/api/notify';
+
+  const headers = new Headers({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': `Bearer ${token}`
+  });
+
+  const body = new URLSearchParams({ message });
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: body
+    });
+
+    if (response.ok) {
+      console.log('訊息發送成功！');
+    } else {
+      console.error('發送失敗！', response.status, await response.text());
+    }
+  } catch (error) {
+    console.error('發送通知時發生錯誤：', error);
+  }
+}
