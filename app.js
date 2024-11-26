e5156a88301536ca863f38540e310f1877299c4f
Retema2200 <s125145757@gmail.com>
2024-11-26 12:19:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ function handleNotification(event) {
       console.log(`濕度數據: ${humValue.toFixed(1)}%`);
       statusDisplay.textContent = `目前溫度：${tempValue.toFixed(1)}°C，濕度：${humValue.toFixed(1)}%`;
 
-      message = `溫度：${tempValue.toFixed(1)}°C，濕度：${humValue.toFixed(1)}%`;
     
       break;
     case 0x02: // 血氧
@@ -39,8 +38,6 @@ function handleNotification(event) {
     console.log(`血氧數據: ${bldValue}%`);
     console.log(`心率數據: ${hbrValue} BPM`);
     statusDisplay.textContent = `目前血氧：${bldValue}% ，心率：${hbrValue} BPM`;
-
-    message = `血氧：${bldValue}% 心率：${hbrValue} BPM`;
       break;
     case 0x03: // 壓力
       const glvValue = value[1] | (value[2] << 8); // 合併低位和高位
@@ -180,31 +177,3 @@ document.querySelector('#measureOxygenButton').addEventListener('click', async (
 }); 
 
 
-
-async function sendLineNotify(message) {
-  const token = 'ZLnjC4GWz4L6MAcKzlB6z6JLNxkfojHjJAUksYrJhND';
-  const url = 'https://notify-api.line.me/api/notify';
-
-  const headers = new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `Bearer ${token}`
-  });
-
-  const body = new URLSearchParams({ message });
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: body
-    });
-
-    if (response.ok) {
-      console.log('訊息發送成功！');
-    } else {
-      console.error('發送失敗！', response.status, await response.text());
-    }
-  } catch (error) {
-    console.error('發送通知時發生錯誤：', error);
-  }
-}
